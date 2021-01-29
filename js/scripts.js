cc4783818cd089323093803fe6206a404c261390
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-01-28 22:18:35 -0800

    Change if...else statement to append winning score.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business Logic
 function Player(name) {
-  this.score = 0;
+  this.score = 90;
   this.name = name;
 }
 function randomNumber(max) {
@@ -27,20 +27,12 @@ $(document).ready(function() {
   player2 = new Player(user2);
   let currentPlayer = player1;
   let currentRoundScore = 0;
-
-  $("#p1-score").append(currentPlayer.score);
-  $("#p2-score").append(currentPlayer.score);
-  
+  $("#P1-score").append(currentPlayer.score);
+  $("#P2-score").append(currentPlayer.score);
   $("#current-player").empty();
   $("#current-player").append(currentPlayer.name);
-
   $("button#roll").click(function() {
     let roll = (randomNumber(6) + 1);
-    if ((currentPlayer.score + currentRoundScore + roll) >= 100) {
-      $("button#roll").hide();
-      $("button#hold").hide();
-      $("div#winner").append("GAME OVER " + currentPlayer.name + " wins!");
-    }
     if (roll === 1) {
       currentPlayer = turnEnd(currentPlayer);
       currentRoundScore = 0;
@@ -48,6 +40,18 @@ $(document).ready(function() {
       $("#current-player").append(currentPlayer.name);
       $("#current-round-score").empty();
       $("#current-round-score").append(currentRoundScore);
+    }
+    else if ((currentPlayer.score + currentRoundScore + roll) >= 100) {
+      $("button#roll").hide();
+      $("button#hold").hide();
+      currentRoundScore += roll;
+      $("#current-round-score").empty();
+      $("#current-round-score").append(currentRoundScore);
+      $("div#winner").append("GAME OVER " + currentPlayer.name + " wins!");
+      addToPlayerScore(currentPlayer, currentRoundScore);
+      $("#" + currentPlayer.name + "-score").empty();
+      $("#" + currentPlayer.name + "-score").append(currentPlayer.score);
+      console.log(currentPlayer.name)
     }
     else {
       currentRoundScore += roll;
@@ -64,8 +68,8 @@ $(document).ready(function() {
     {
       addToPlayerScore(currentPlayer, currentRoundScore);
       $("#current-roll").empty();
-      $("#p1-score").empty();
-      $("#p1-score").append(currentPlayer.score);
+      $("#P1-score").empty();
+      $("#P1-score").append(currentPlayer.score);
       currentRoundScore = 0;
       $("#current-round-score").empty();
       $("#current-round-score").append(currentRoundScore);
@@ -73,8 +77,8 @@ $(document).ready(function() {
     else {
       addToPlayerScore(currentPlayer, currentRoundScore);
       $("#current-roll").empty();
-      $("#p2-score").empty();
-      $("#p2-score").append(currentPlayer.score);
+      $("#P2-score").empty();
+      $("#P2-score").append(currentPlayer.score);
       currentRoundScore = 0;
       $("#current-round-score").empty();
       $("#current-round-score").append(currentRoundScore);
@@ -91,10 +95,10 @@ $(document).ready(function() {
     player2.score = 0;
     currentRoundScore = 0;
     roll = 0;
-    $("#p1-score").empty();
-    $("#p1-score").append(currentPlayer.score);
-    $("#p2-score").empty();
-    $("#p2-score").append(currentPlayer.score);
+    $("#P1-score").empty();
+    $("#P1-score").append(currentPlayer.score);
+    $("#P2-score").empty();
+    $("#P2-score").append(currentPlayer.score);
     $("#current-round-score").empty();
     $("#current-round-score").append(currentRoundScore);
     $("#current-roll").empty();
